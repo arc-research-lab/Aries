71fcfe7da9289cb56ebd2667970d36e519563d03
“JinmingZhuang” <m13688033757@163.com>
2025-04-06 02:49:16 -0400

    [Transform][AriesPLSerialize] Update PL serialization

--- lib/Transform/aries/AriesPLSerialize.cpp
+++ lib/Transform/aries/AriesPLSerialize.cpp
@@ -215,7 +215,14 @@ private:
     }
   }
 
-  // This function will update the arguments with dynamic shape in adf.pl funcs
+  // This function will update the arguments with dynamic shape in adf.pl 
+  // top_func, origin_func and top_host funcs
+  // Then update the uses of these arguments, now handle the affine.load/store
+  // ops and CastOps
+  // TODO:: May need to deal with other ops that uses the updated arg
+  // May need to handle the definingOp of caller, since the callee has been
+  // updated, now assumes the updated operands are all in block argument or 
+  // defined by CastOp
   bool typeUpdate(OpBuilder builder, FuncOp func){
     // Traverse the arguments and replace the ones with dynamic shape
     auto inTypes =SmallVector<Type,8>(func.getArgumentTypes().begin(),
@@ -250,9 +257,11 @@ private:
         }
       }
     }
-    for (unsigned i=0; i < func.getNumArguments(); i++){
-      auto arg = args[i];
-      auto memType = dyn_cast<MemRefType>(arg.getType());
+    // Update the callee function type.
+    SmallVector<int64_t, 4> argIds;
+    for(unsigned i=0; i < inTypes.size(); i++){
+      auto inType = inTypes[i];
+      auto memType = dyn_cast<MemRefType>(inType); 
       if(!memType)
         continue;
       auto memAttr = memType.getMemorySpace();
@@ -261,31 +270,41 @@ private:
         if(!IntAttr || IntAttr.getInt()!=(int)MemorySpace::L3)
           continue;
       }
+      auto eleType = memType.getElementType();
+      auto newType = MemRefType::get({ShapedType::kDynamic},eleType);
+      // The top_host and top_func has static memory size
+      // In top_host, it needs to keep the shape 
+      // In top_func, it needs to change to dynamic size
+      if(memType.hasStaticShape()){
+        auto numElem = memType.getNumElements();
+        if(func->hasAttr("top_func"))
+          inTypes[i] = newType;
+        else
+          inTypes[i] = MemRefType::get({numElem},eleType);
+      }else{
+        inTypes[i] = newType;
+      }
+      argIds.push_back(i);
+    }
+    func.setType(builder.getFunctionType(inTypes, outTypes));
+    // For private functions return directly
+    if(func.getVisibility() == SymbolTable::Visibility::Private)
+      return true;
+    
+    // Update the block argument type and update the affine.load/store op that
+    // uses the argument
+    for (auto argId : argIds){
+      auto arg = func.getArgument(argId);
+      auto memType = dyn_cast<MemRefType>(arg.getType());
       SmallVector<int64_t, 4> memSizes(memType.getShape());
       auto eleType = memType.getElementType();
       auto width = eleType.getIntOrFloatBitWidth();
-      auto newType = MemRefType::get({ShapedType::kDynamic},eleType);
-      // Handle CastOp
-      if(memType.hasStaticShape()){
-        MemRefType newArgType;
-        bool top_flag;
-        auto numElem = memType.getNumElements();
-        if(!func->hasAttr("top_func")){
-          newArgType = MemRefType::get({numElem},eleType);
-          top_flag =false;
-        }else{
-          newArgType = newType;
-          top_flag =true;
-        }
-        inTypes[i] = newArgType;
-        arg.setType(newArgType); 
-        castUpdate(builder, newType, arg, top_flag);
-        continue;
-      }
-      inTypes[i] = newType;
+      // Update argument type
+      auto newType = inTypes[argId];
       arg.setType(newType);
+      // Get the pack number
       unsigned packNum;
-      if(!getPacksize(typeAttrs, width, i, packNum, idxs)){
+      if(!getPacksize(typeAttrs, width, argId, packNum, idxs)){
         llvm::errs() << "Find dynamic memref not in the arguments\n";
         return false;
       }
@@ -294,8 +313,7 @@ private:
       for (auto use: llvm::make_early_inc_range(arg.getUsers())){
         if(dyn_cast<CallOp>(use)){
           continue;
-        }
-        else if(auto load = dyn_cast<AffineLoadOp>(use)){
+        }else if(auto load = dyn_cast<AffineLoadOp>(use)){
           AffineStoreOp store;
           if(!memUpdate(builder, entryBlock, load, store, packNum, memSizes, 
              args, metaData)){
@@ -307,14 +325,27 @@ private:
              args, metaData)){
             return false;
           }
+        }else if(auto castOp = dyn_cast<CastOp>(use)){
+          // For castOp in top_func, need to replace it with the updated arg
+          // For castOp in top_host need to create new cast using new arg
+          auto res = castOp.getResult();
+          if(func->hasAttr("top_func")){ 
+            res.replaceAllUsesWith(arg);
+            use->erase();
+          }else{
+            builder.setInsertionPoint(castOp);
+            auto loc = builder.getUnknownLoc();
+            auto newType = MemRefType::get({ShapedType::kDynamic}, eleType);
+            auto newCast = builder.create<CastOp>(loc, newType, arg);
+            res.replaceAllUsesWith(newCast);
+            use->erase();
+          }
         }else{
           llvm::errs() << "Argument with dynamic size is used by other ops\n";
           return false;
         }
       }
     }
-    // Update the callee function type.
-    func.setType(builder.getFunctionType(inTypes, outTypes));
     return true;
   }
 
@@ -325,8 +356,8 @@ private:
   bool PLSerialize (ModuleOp mod) {
     auto builder = OpBuilder(mod);
     for (auto func: mod.getOps<FuncOp>()){
-      if(!func->hasAttr("adf.pl") && !func->hasAttr("adf.func")
-      && !func->hasAttr("top_func") && !func->hasAttr("origin_func"))
+      if(!func->hasAttr("adf.pl")   && !func->hasAttr("adf.func") &&
+         !func->hasAttr("top_func") && !func->hasAttr("origin_func"))
         continue;
       if(!typeUpdate(builder, func))
         return false;
