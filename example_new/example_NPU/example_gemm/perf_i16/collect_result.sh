92983b46e7da406f358dd34a7545291d35258dff
“JinmingZhuang” <m13688033757@163.com>
2025-05-14 02:03:38 -0400

    [Example] Update perf_i16

--- example_new/example_NPU/example_gemm/perf_i16/collect_result.sh
+++ example_new/example_NPU/example_gemm/perf_i16/collect_result.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "Directory, NPU execution time" > summary_results.txt
+echo "Directory, Test Result, NPU Execution Time" > summary_results.txt
 for dir in gemm_*/project; do
   log_file="$dir/result.log"
-  [ -f "$log_file" ] && grep -m 1 "NPU execution time:" "$log_file" | \
-  awk -v d=$(basename "$(dirname "$dir")") '{print d ", " $0}' >> summary_results.txt
+  [ -f "$log_file" ] && { 
+    test_result=$(grep -m 1 "^TEST" "$log_file")
+    npu_time=$(grep -m 1 "^NPU execution time:" "$log_file")
+    echo "$(basename "$(dirname "$dir")"), \"$test_result\", \"$npu_time\"" >> summary_results.txt
+  }
 done
 echo "Collected results saved to summary_results.txt"
